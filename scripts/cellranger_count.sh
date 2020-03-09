f809c9152c1b14c390d62f2cb09cd628b29694d8
rpolicastro <rpolicas@indiana.edu>
2020-03-09 10:45:46 -0400

    updating cellranger count script

--- scripts/cellranger_count.sh
+++ scripts/cellranger_count.sh
@@ -3,7 +3,7 @@
 ## Variables
 
 workdir=/N/dc2/scratch/rpolicas/sam_scRNAseq
-container=
+container=scrnaseq_software_cellranger_3.1.0.sif
 
 ###############################
 ## CellRanger Count of Reads ##
@@ -20,25 +20,29 @@ module load singularity/3.5.2
 
 ## Download singularity container.
 
-if [ ! -f /N/dc2/scratch/rpolicas/sam_scRNAseq/${container} ]; then
-   singularity pull
+if [ ! -f ${workdir}/${container} ]; then
+   singularity pull --arch amd64 library://rpolicastro/default/scrnaseq_software:cellranger_3.1.0
 fi
 
 ## Download 10X hg38 genome.
 
-wget -O ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0.tar.gz \
-http://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-GRCh38-3.0.0.tar.gz
+if [ ! -d ${workdir}/genome ]; then
+   mkdir -p ${workdir}/genome
+fi
 
-cd genome
-tar -xzvf refdata-cellranger-GRCh38-3.0.0.tar.gz
-cd $workdir
+if [ ! -d ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 ]; then
+   cd ${workdir}/genome
+   wget http://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-GRCh38-3.0.0.tar.gz
+   tar -xzvf refdata-cellranger-GRCh38-3.0.0.tar.gz
+   cd ${workdir}
+fi
 
 ## Run CellRanger
 ## ----------
 
 ## Count H508 EV
 
-singularity exec -eCB $workdir -H $workdir \
+singularity exec -eCB $workdir -H $workdir $container \
 cellranger count \
    --id H508_EV \
    --fastqs ${workdir}/sequences/H508/BL_Chrm_001_Ohagan_1 \
@@ -48,7 +52,7 @@ cellranger count \
 
 ## Count H508 LSD1 KD
 
-singularity exec -eCB $workdir -H $workdir \
+singularity exec -eCB $workdir -H $workdir $container \
 cellranger count \
    --id H508_LSD1_KD \
    --fastqs ${workdir}/sequences/H508/BL_Chrm_001_Ohagan_2 \
@@ -58,7 +62,7 @@ cellranger count \
 
 ## Count HT29 EV
 
-singularity exec -eCB $workdir -H $workdir \
+singularity exec -eCB $workdir -H $workdir $container \
 cellranger count \
    --id HT29_EV \
    --fastqs ${workdir}/sequences/HT29/Chrm_066_OHagan_BL_1 \
@@ -68,7 +72,7 @@ cellranger count \
 
 ## Count HT29 LSD1 KD
 
-singularity exec -eCB $workdir -H $workdir \
+singularity exec -eCB $workdir -H $workdir $container \
 cellranger count \
    --id HT29_LSD1_KD \
    --fastqs ${workdir}/sequences/HT29/Chrm_066_OHagan_BL_2 \
