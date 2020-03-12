c51e2d7acbb654533affd7a1a5845ce24b92543e
rpolicastro <rpolicas@indiana.edu>
2020-03-12 09:13:36 -0400

    adding Wang et al., 2019 Colon scRNA-seq samples

--- scripts/cellranger_count.sh
+++ scripts/cellranger_count.sh
@@ -42,40 +42,60 @@ fi
 
 ## Count H508 EV
 
-singularity exec -eCB $workdir -H $workdir $container \
-cellranger count \
-   --id H508_EV \
-   --fastqs ${workdir}/sequences/H508/BL_Chrm_001_Ohagan_1 \
-   --sample EV \
-   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
-   --expect-cells 10000
+#singularity exec -eCB $workdir -H $workdir $container \
+#cellranger count \
+#   --id H508_EV \
+#   --fastqs ${workdir}/sequences/H508/BL_Chrm_001_Ohagan_1 \
+#   --sample EV \
+#   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
+#   --expect-cells 10000
 
 ## Count H508 LSD1 KD
 
-singularity exec -eCB $workdir -H $workdir $container \
-cellranger count \
-   --id H508_LSD1_KD \
-   --fastqs ${workdir}/sequences/H508/BL_Chrm_001_Ohagan_2 \
-   --sample KO \
-   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
-   --expect-cells 10000
+#singularity exec -eCB $workdir -H $workdir $container \
+#cellranger count \
+#   --id H508_LSD1_KD \
+#   --fastqs ${workdir}/sequences/H508/BL_Chrm_001_Ohagan_2 \
+#   --sample KO \
+#   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
+#   --expect-cells 10000
 
 ## Count HT29 EV
 
-singularity exec -eCB $workdir -H $workdir $container \
-cellranger count \
-   --id HT29_EV \
-   --fastqs ${workdir}/sequences/HT29/Chrm_066_OHagan_BL_1 \
-   --sample sm_ssrna1 \
-   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
-   --expect-cells 10000
+#singularity exec -eCB $workdir -H $workdir $container \
+#cellranger count \
+#   --id HT29_EV \
+#   --fastqs ${workdir}/sequences/HT29/Chrm_066_OHagan_BL_1 \
+#   --sample sm_ssrna1 \
+#   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
+#   --expect-cells 10000
 
 ## Count HT29 LSD1 KD
 
+#singularity exec -eCB $workdir -H $workdir $container \
+#cellranger count \
+#   --id HT29_LSD1_KD \
+#   --fastqs ${workdir}/sequences/HT29/Chrm_066_OHagan_BL_2 \
+#   --sample sm_ssrna2 \
+#   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
+#   --expect-cells 10000
+
+## Count Colon 1
+
 singularity exec -eCB $workdir -H $workdir $container \
 cellranger count \
-   --id HT29_LSD1_KD \
-   --fastqs ${workdir}/sequences/HT29/Chrm_066_OHagan_BL_2 \
-   --sample sm_ssrna2 \
+   --id COLON_1 \
+   --fastqs ${workdir}/sequences/colon_1 \
+   --sample SRR8513796 \
+   --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
+   --expect-cells 10000
+
+## Count Colon 2
+
+singularity exec -eCB $workdir -H $workdir $container \
+cellranger count \
+   --id COLON_2 \
+   --fastqs ${workdir}/sequences/colon_2 \
+   --sample SRR8513797 \
    --transcriptome ${workdir}/genome/refdata-cellranger-GRCh38-3.0.0 \
    --expect-cells 10000
