84a32824b5a2ad82d10f9c0227bdb2d0815ee03e
rpolicastro <rpolicas@indiana.edu>
2020-05-21 10:41:14 -0400

    fixing code for marker genes from custom clusters

--- scripts/seurat_marker_genes.R
+++ scripts/seurat_marker_genes.R
@@ -7,13 +7,14 @@ library("Seurat")
 
 ## For raw clusters.
 
-#seurat_integrated <- readRDS(file.path("results", "r_objects", "seurat_integrated.RDS"))
-#markers <- FindAllMarkers(seurat_integrated, assay = "RNA", min.pct = 0.25)
-#saveRDS(markers, file.path("results", "r_objects", "markers.RDS"))
+seurat_integrated <- readRDS(file.path("results", "r_objects", "seurat_integrated.RDS"))
+markers <- FindAllMarkers(seurat_integrated, assay = "RNA", min.pct = 0.25)
+saveRDS(markers, file.path("results", "r_objects", "markers.RDS"))
 
 ## For custom clusters.
 
 seurat_expanded <- readRDS(file.path("results", "r_objects", "seurat_expanded.RDS"))
-Idents(seurat_expanded) <- "custom_markers"
+Idents(seurat_expanded) <- "custom_clusters"
+
 custom_markers <- FindAllMarkers(seurat_expanded, assay = "RNA", min.pct = 0.25)
 saveRDS(custom_markers, file.path("results", "r_objects", "custom_markers.RDS"))
