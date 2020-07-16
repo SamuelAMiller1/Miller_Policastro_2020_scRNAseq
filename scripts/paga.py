effe541a6a8cb724b5ea37ff79ca9fca25d9587b
rpolicastro <rpolicas@indiana.edu>
2020-07-15 21:19:32 -0400

    updating scVelo and PAGA scripts

--- scripts/paga.py
+++ scripts/paga.py
@@ -4,26 +4,28 @@ import numpy as np
 import pandas as pd
 import scvelo as scv
 from pathlib import Path
+import pickle
+import os
 
 ## Regular Data
 ## ----------
 
 ## Open the file.
 
-results = Path("results/py_objects/normal_seurat.h5ad")
-sc_data = sc.read_h5ad(results)
+#results = Path("results/py_objects/normal_seurat.h5ad")
+#sc_data = sc.read_h5ad(results)
 
-sc_data.obs = sc_data.obs.astype('category')
+#sc_data.obs = sc_data.obs.astype('category')
 
 ## preprocess data.
 
-sc.tl.pca(sc_data, svd_solver='arpack')
-sc.pp.neighbors(sc_data, n_neighbors=4, n_pcs=30)
+#sc.tl.pca(sc_data, svd_solver='arpack')
+#sc.pp.neighbors(sc_data, n_neighbors=4, n_pcs=30)
 
 ## Run PAGA.
 
-sc.tl.paga(sc_data, groups='integrated_snn_res.0.6')
-sc.pl.paga_compare(sc_data)
+#sc.tl.paga(sc_data, groups='integrated_snn_res.0.6')
+#sc.pl.paga_compare(sc_data)
 
 ## Recompute embedding using PAGA.
 
@@ -33,57 +35,140 @@ sc.pl.paga_compare(sc_data)
 ## Regular Modeling
 ## ----------
 
-## Load data.
+## Preparing sample data.
 
-results = Path("results/py_objects/H508_LSD1_KD_seurat.h5ad")
-scv_data = scv.read(results)
+samples = {
+  'COLON_1' : 'results/py_objects/COLON_1_seurat.h5ad',
+  'HT29_EV' : 'results/py_objects/HT29_EV_seurat.h5ad',
+  'HT29_LSD1_KD' : 'results/py_objects/HT29_LSD1_KD_seurat.h5ad',
+  'H508_EV' : 'results/py_objects/H508_EV_seurat.h5ad',
+  'H508_LSD_KD' : 'results/py_objects/HT29_EV_seurat.h5ad'
+}
 
-scv_data.obs = scv_data.obs.astype('category')
+samples = {x:scv.read(y) for x,y in samples.items()}
+
+## Change the metadata to categorical.
+
+for key in samples.keys():
+	samples[key].obs = samples[key].obs.astype('category')
 
 ## Preprocess the data.
 
-scv.pp.filter_and_normalize(scv_data, min_shared_counts=20, n_top_genes=3000)
-scv.pp.moments(scv_data, n_pcs=30, n_neighbors=30)
+for key in samples.keys():
+	scv.pp.filter_and_normalize(samples[key], min_shared_counts=20, n_top_genes=3000)
+	scv.pp.moments(samples[key], n_pcs=30, n_neighbors=30)
 
 ## Calculate RNA velocities.
 
-#scv.tl.recover_dynamics(scv_data)
-scv.tl.velocity(scv_data)#,mode="dynamical")
-scv.tl.velocity_graph(scv_data)
+for key in samples.keys():
+	scv.tl.velocity(samples[key])
+	scv.tl.velocity_graph(samples[key])
 
-## Plot RNA velocities.
+## Save the velocities.
 
-scv.pl.velocity_embedding_stream(scv_data, basis='umap', color='integrated_snn_res.0.6')
+with open('results/py_objects/velocities.pickle', 'wb') as handle:
+	pickle.dump(samples, handle)
 
-scv.pl.velocity_embedding(
-  scv_data, arrow_length=3, arrow_size=2, dpi=120,
-  basis ='umap', color='integrated_snn_res.0.6'
-)
+## Load the velocities if required.
 
-## Latent time.
+with open('results/py_objects/velocities.pickle', 'rb') as handle:
+	samples = pickle.load(handle)
 
-#scv.tl.latent_time(scv_data)
-#scv.pl.scatter(scv_data, color='latent_time', color_map='gnuplot', size=80)
+## Plot RNA velocity streams.
+
+outdir = 'results/trajectory/velocity/velocity_plots'
+if not os.path.exists(outdir):
+	os.makedirs(outdir)
+
+scv.settings.figdir = outdir
+
+for key,value in samples.items():
+	scv.pl.velocity_embedding_stream(
+	  value, basis='umap', color='integrated_snn_res.0.6',
+	  save = '%s.png' % key, title = key, show = False,
+	  figsize = (10, 10), size = 50, dpi = 300
+	)
+
+## Plot RNA velocity grids.
+
+outdir = 'results/trajectory/velocity/velocity_arrows'
+if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+scv.settings.figdir = outdir
+
+for key,value in samples.items():
+	scv.pl.velocity_embedding(
+	  value, arrow_length=3, arrow_size=2, dpi=300,
+	  basis ='umap', color='integrated_snn_res.0.6',
+	  figsize = (10, 10), size = 50, show = False,
+	  save = '%s.png' % key, title = key
+	)
+
+## Plot velocity speed and coherence.
+
+outdir = 'results/trajectory/velocity/velocity_speed_coherence'
+if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+scv.settings.figdir = outdir
+
+metrics = ['velocity_length', 'velocity_confidence']
+for key,value in samples.items():
+	scv.tl.velocity_confidence(value)
+	scv.pl.scatter(
+	  value, c = metrics, cmap = 'coolwarm', perc=[5, 95],
+	  size = 50, show = False, dpi = 300, figsize = (10, 10),
+	  save = '%s.png' % key
+	)
+
+## Plot cell connections.
+
+outdir = 'results/trajectory/velocity/velocity_connections'
+if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+scv.settings.figdir = outdir
+
+for key,value in samples.items():
+	scv.pl.velocity_graph(
+	  value, threshold = .2, size = 50, show = False, dpi = 300,
+	  figsize = (10, 10), color = 'integrated_snn_res.0.6',
+	  save = '%s.png' % key, title = key
+	)
+
+## Plot velocity pseudotime.
+
+outdir = 'results/trajectory/velocity/velocity_pseudotime'
+if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+scv.settings.figdir = outdir
+
+for key,value in samples.items():
+	scv.tl.velocity_pseudotime(value)
+	scv.pl.scatter(
+	  value, color='velocity_pseudotime', cmap='gnuplot', dpi = 300,
+	  show = False, figsize = (10, 10), title = key, size = 50,
+	  save = '%s.png' % key
+	)
 
 ## PAGA.
 
-scv_data.uns['neighbors']['distances'] = scv_data.obsp['distances']
-scv_data.uns['neighbors']['connectivities'] = scv_data.obsp['connectivities']
+outdir = 'results/trajectory/velocity/velocity_paga'
+if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-scv.tl.paga(scv_data, groups='integrated_snn_res.0.6')
-scv.pl.paga(scv_data, basis='umap', color='integrated_snn_res.0.6')
+scv.settings.figdir = outdir
 
-## Important dynamical genes.
+for key in samples.keys():
+	samples[key].uns['neighbors']['distances'] = samples[key].obsp['distances']
+	samples[key].uns['neighbors']['connectivities'] = samples[key].obsp['connectivities']
 
-#scv.tl.rank_dynamical_genes(scv_data, groupby='integrated_snn_res.0.6')
-
-#df = scv.get_df(scv_data, 'rank_dynamical_genes/names')
-#df.head(5)
-
-## Important Genes.
-
-scv.tl.rank_velocity_genes(scv_data, groupby='integrated_snn_res.0.6', min_corr=.3)
-
-## Recompute embedding using PAGA.
-
-sc.tl.draw_graph(scv_data, init_pos='paga')
+for key,value in samples.items():
+	scv.tl.paga(value, groups = 'integrated_snn_res.0.6')
+	scv.pl.paga(
+	  value, basis = 'umap', color = 'integrated_snn_res.0.6',
+	  dpi = 200, show = False, figsize = (10, 10), title = key, size = 50,
+	  save = '%s.png' % key
+	)
