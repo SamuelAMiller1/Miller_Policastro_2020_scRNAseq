c532cabdc99d14b86a3caa70cf1629b9c0483a3d
rpolicastro <rpolicas@indiana.edu>
2020-07-20 17:20:04 -0400

    switching to resultion of 0.7 and making targeted gene plots for TFF3 and MUC2

--- scripts/paga.py
+++ scripts/paga.py
@@ -7,6 +7,10 @@ from pathlib import Path
 import pickle
 import os
 
+## Variables.
+
+clusters = 'integrated_snn_res.0.7'
+
 ##################
 ## RNA Velocity ##
 ##################
@@ -50,7 +54,7 @@ scv.settings.figdir = outdir
 
 for key,value in samples.items():
     scv.pl.velocity_embedding_stream(
-      value, basis='umap', color='integrated_snn_res.0.6',
+      value, basis='umap', color=clusters,
       save = '%s.png' % key, title = key, show = False,
       figsize = (10, 10), size = 50, dpi = 300
     )
@@ -66,7 +70,7 @@ scv.settings.figdir = outdir
 for key,value in samples.items():
     scv.pl.velocity_embedding(
       value, arrow_length=3, arrow_size=2, dpi=300,
-      basis ='umap', color='integrated_snn_res.0.6',
+      basis ='umap', color=clusters,
       figsize = (10, 10), size = 50, show = False,
       save = '%s.png' % key, title = key
     )
@@ -99,7 +103,7 @@ scv.settings.figdir = outdir
 for key,value in samples.items():
     scv.pl.velocity_graph(
       value, threshold = .2, size = 50, show = False, dpi = 300,
-      figsize = (10, 10), color = 'integrated_snn_res.0.6',
+      figsize = (10, 10), color = clusters,
       save = '%s.png' % key, title = key
     )
 
@@ -132,9 +136,9 @@ for key in samples.keys():
     samples[key].uns['neighbors']['connectivities'] = samples[key].obsp['connectivities']
 
 for key,value in samples.items():
-    scv.tl.paga(value, groups = 'integrated_snn_res.0.6')
+    scv.tl.paga(value, groups = clusters)
     scv.pl.paga(
-      value, basis = 'umap', color = 'integrated_snn_res.0.6',
+      value, basis = 'umap', color = clusters,
       dpi = 200, show = False, figsize = (10, 10), title = key, size = 50,
       save = '%s.png' % key
     )
@@ -146,7 +150,7 @@ if not os.path.exists(outdir):
     os.makedirs(outdir)
 
 for value in samples.values():
-    scv.tl.rank_velocity_genes(value, groupby = 'integrated_snn_res.0.6', min_corr=.3)
+    scv.tl.rank_velocity_genes(value, groupby = clusters, min_corr=.3)
 
 for key,value in samples.items():
     df = scv.DataFrame(value.uns['rank_velocity_genes']['names'])
@@ -176,7 +180,7 @@ comparisons = [
 diff_samples = {}
 for x,y in  comparisons:
     comp = samples[x].concatenate(samples[y])
-    comp.obs['groups'] = comp.obs[['orig.ident', 'integrated_snn_res.0.6']].astype(str).agg('_'.join, axis = 1)
+    comp.obs['groups'] = comp.obs[['orig.ident', clusters]].astype(str).agg('_'.join, axis = 1)
     diff_samples['{}_vs_{}'.format(x, y)] = comp
 
 for value in diff_samples.values():
@@ -188,23 +192,86 @@ for key,value in diff_samples.items():
 
 ## Targeted gene plot.
 
-df = scv.DataFrame(samples['HT29_EV'].uns['rank_velocity_genes']['names'])
+outdir = 'results/trajectory/velocity/velocity_targeted_genes'
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 
-HT29_EV = samples['HT29_EV']
-HT29_EV.obs['integrated_snn_res.0.6'] = HT29_EV.obs['integrated_snn_res.0.6'].astype(str)
+scv.settings.figdir = outdir
 
-kwargs = dict(
-  frameon=False, size=10, linewidth=1.5,
-  add_outline='11, 10, 17'
-)
+genes = ['MUC2', 'TFF3']
 
-scv.pl.scatter(HT29_EV, 'PLCB4', color='integrated_snn_res.0.6')
+for key,value in samples.items():
+    scv.pl.velocity(
+      value, genes, dpi = 300, show = False, figsize = (10, 10),
+      size = 20, save = '{}_{}_{}.png'.format(key, genes[0], genes[1])
+    )
 
 ## Cell fate trace
 
-x, y = scv.utils.get_cell_transitions(HT29_EV, basis='umap', starting_cell=70)
-ax = scv.pl.velocity_graph(HT29_EV, c='lightgrey', edge_width=.05, show=False)
-ax = scv.pl.scatter(HT29_EV, x=x, y=y, s=120, c='ascending', cmap='gnuplot', ax=ax)
+select_clusts = ['8', '13', '9']
+select_clusts = ['17', '10']
+clusts = {x:samples['HT29_EV'].obs[samples['HT29_EV'].obs[clusters] == x].index for x in select_clusts}
+
+for key,value in clusts.items():
+    outdir = 'results/trajectory/velocity/velocity_trace/cluster_{}'.format(key)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    scv.settings.figdir = outdir
+    for c in value:
+        cell = samples['HT29_EV'].obs.index.get_loc(c)
+        x,y = scv.utils.get_cell_transitions(samples['HT29_EV'], basis='umap', starting_cell=cell)
+        ax = scv.pl.umap(samples['HT29_EV'], show=False)
+        scv.pl.scatter(
+          samples['HT29_EV'], x=x, y=y, s=120, c='ascending', cmap='gnuplot',
+          ax=ax, title = key, show = False,
+          save = 'cluster{}_cell{}.png'.format(key, cell)
+        )
+
+###################
+## Joint Analysis ##
+###################
+
+## Concatenate related samples.
+
+joint = {
+  'EV' : samples['HT29_EV'].concatenate(samples['H508_EV']),
+  'LSD1_KD' : samples['HT29_LSD1_KD'].concatenate(samples['H508_LSD1_KD'])
+}
+
+## Find important genes.
+
+outdir = 'results/trajectory/velocity_joint/velocity_genes'
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+for value in joint.values():
+    scv.tl.rank_velocity_genes(value, groupby = clusters, min_corr=.3)
+
+for key,value in joint.items():
+    df = scv.DataFrame(value.uns['rank_velocity_genes']['names'])
+    df.to_csv("{}/{}.tsv".format(outdir, key), sep = '\t', header = True, index = False)
+
+## Genes different between EV and LSD1_KD.
+
+#outdir = 'results/trajectory/velocity_joint/velocity_diff_genes'
+#if not os.path.exists(outdir):
+#    os.makedirs(outdir)
+
+#comparisons = [('EV', 'LSD1_KD')]
+
+#diff_samples = {}
+#for x,y in  comparisons:
+#    comp = joint[x].concatenate(joint[y])
+#    comp.obs['groups'] = comp.obs[['orig.ident', clusters]].astype(str).agg('_'.join, axis = 1)
+#    diff_samples['{}_vs_{}'.format(x, y)] = comp
+
+#for value in diff_samples.values():
+#    scv.tl.rank_velocity_genes(value, groupby = 'groups', min_corr=.3)
+
+#for key,value in diff_samples.items():
+#    df = scv.DataFrame(value.uns['rank_velocity_genes']['names'])
+#    df.to_csv("{}/{}.tsv".format(outdir, key), sep = '\t', header = True, index = False)
+
 
 #####################
 ## Dynamical Model ##
@@ -255,7 +322,7 @@ scv.settings.figdir = outdir
 
 for key,value in samples.items():
     scv.pl.velocity_embedding_stream(
-      value, basis='umap', color='integrated_snn_res.0.6',
+      value, basis='umap', color=clusters,
       save = '%s.png' % key, title = key, show = False,
       figsize = (10, 10), size = 50, dpi = 300
     )
@@ -271,7 +338,7 @@ scv.settings.figdir = outdir
 for key,value in samples.items():
     scv.pl.velocity_embedding(
       value, arrow_length=3, arrow_size=2, dpi=300,
-      basis ='umap', color='integrated_snn_res.0.6',
+      basis ='umap', color=clusters,
       figsize = (10, 10), size = 50, show = False,
       save = '%s.png' % key, title = key
     )
@@ -301,7 +368,7 @@ if not os.path.exists(outdir):
 scv.settings.figdir = outdir
 
 for key,value in samples.items():
-    scv.tl.rank_dynamical_genes(value, groupby = 'integrated_snn_res.0.6')
+    scv.tl.rank_dynamical_genes(value, groupby = clusters)
     df = scv.get_df(value, 'rank_dynamical_genes/names')
     df.to_csv("{}/{}.tsv".format(outdir, key), sep = '\t', header = True, index = False)
 
